a34fa89f0eeb5fbbfd18a207afe42136b3e3fe03
Mohamed Mustafa Banni <mohamedmustafa.banni@student.griffith.ie>
2026-09-20 17:39:11 +0200

    fix(user): fix status lookup data type

--- backend/src/main/java/com/mbanni/shop/user/UserRepository.java
+++ backend/src/main/java/com/mbanni/shop/user/UserRepository.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
-    List<User> findByStatus(String status);
+    List<User> findByStatus(UserStatus status);
     boolean existsByEmail(String email);
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
