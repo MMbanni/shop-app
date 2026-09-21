c5cdb3c81e87eb5a81fadebbe33625269e7f013f
Mohamed Mustafa Banni <mohamedmustafa.banni@student.griffith.ie>
2026-09-21 06:18:43 +0200

    fix(user): add validation to dto

--- backend/src/main/java/com/mbanni/shop/user/dto/UpdateUserRequestDto.java
+++ backend/src/main/java/com/mbanni/shop/user/dto/UpdateUserRequestDto.java
@@ -1,8 +1,14 @@
 package com.mbanni.shop.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+
 public record UpdateUserRequestDto (
-    String name,
-    String email
+        @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
+        String name,
+        @Email(message = "Email must be valid")
+        @Size(max = 100, message = "Email must be at most 100 characters")
+        String email
 ){}
 
 
--- backend/src/main/java/com/mbanni/shop/user/dto/UpdateUserStatusRequestDto.java
+++ backend/src/main/java/com/mbanni/shop/user/dto/UpdateUserStatusRequestDto.java
@@ -1,8 +1,10 @@
 package com.mbanni.shop.user.dto;
 
 import com.mbanni.shop.user.UserStatus;
+import jakarta.validation.constraints.NotNull;
 
 public record UpdateUserStatusRequestDto(
+        @NotNull(message = "Status required")
         UserStatus status,
         int duration
 ) {
