13864471db85cf6eedf110b53b33324a18c83f5e
Mohamed Mustafa Banni <mohamedmustafa.banni@student.griffith.ie>
2026-09-23 23:29:53 +0200

    fix(product): allow updating description

--- backend/src/main/java/com/mbanni/shop/product/dto/UpdateProductRequestDto.java
+++ backend/src/main/java/com/mbanni/shop/product/dto/UpdateProductRequestDto.java
@@ -2,12 +2,14 @@ package com.mbanni.shop.product.dto;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
+
 import java.math.BigDecimal;
 
 public record UpdateProductRequestDto(
         String name,
         @Min(0) Integer stock,
         @Min(0) BigDecimal price,
-        @Max(500)
+        @Size(max = 500, message = "Description must be at most 500 characters")
         String description
 ) {}
